bff3b838acf95e0488539725c579ba7bfe007af4
Olga <olga.gorbacheva@phystech.edu>
2013-03-02 22:03:30 +0400

    Fixed error handling

--- classVector.h
+++ classVector.h
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <stdlib.h>
 #define MIN_MEMORY_SIZE 200
-//#define UsingType int
 
 using namespace std;
 
@@ -22,103 +21,19 @@ private:
     unsigned int VectorSize; //number of elements
     unsigned int MemorySize; //number of bytes allocated from heap
     UsingType* Pointer;  //pointer to begin of vector
-    int MemoryGrow() //to reallocate memory
-    {
-        UsingType* BufPointer;
-        try{
-            BufPointer = (UsingType*) new char[MemorySize * 2];
-        }
-        catch(...){
-            cerr << "Memory allocation error" << endl;
-            return 1;
-        }
-        memcpy(BufPointer, Pointer, MemorySize);
-        MemorySize = MemorySize * 2;
-        Pointer = BufPointer;
-        return 0;
-    }
+    void MemoryGrow(); //to reallocate memory
 public:
-    explicit vector(): VectorSize(0),
-                       MemorySize(MIN_MEMORY_SIZE * sizeof(UsingType))
-    {
-        Pointer = (UsingType*) new char[MemorySize];
-    }
-
-    explicit vector(unsigned int _MemorySize): VectorSize(0),
-                                               MemorySize(_MemorySize * sizeof(UsingType))
-    {
-        Pointer = (UsingType*) new char[MemorySize];
-    }
-
-    explicit vector(my::vector<UsingType> const & _vector): VectorSize(_vector.VectorSize),
-                                                            MemorySize(_vector.MemorySize)
-    {
-        Pointer = (UsingType*) new char[MemorySize];
-        for (unsigned int i = 0; i < VectorSize; i++){
-            new (Pointer + i) UsingType();
-            Pointer[i] = _vector[i];
-        }
-    }
-
-    ~vector()
-    {
-        delete[] Pointer;
-    }
-
-    void operator=(my::vector<UsingType> const & _vector)
-    {
-        UsingType* BufPointer;
-        BufPointer = (UsingType*) new char[_vector.MemorySize];
-        delete[] Pointer;
-        Pointer = BufPointer;
-        MemorySize = _vector.MemorySize;
-        VectorSize = _vector.VectorSize;
-        for (unsigned int i = 0; i < VectorSize; i++){
-            new (Pointer + i) UsingType();
-            Pointer[i] = _vector[i];
-        }
-    }
-
-    UsingType const & operator[](int const k) const
-    {
-        if (k < 0)
-            throw "Unacceptable index";
-        if ((unsigned int)k > VectorSize)
-            throw "Unacceptable index";
-        UsingType const & Obj = Pointer[k];
-        return Obj;
-    }
-
-    UsingType & operator[](int const k)
-    {
-        if (k < 0)
-            throw "Unacceptable index";
-        if ((unsigned)k >= VectorSize){
-            while (k * sizeof(UsingType) >= MemorySize){
-                if (MemoryGrow())
-                    abort();
-            }
-            for (;VectorSize <= (unsigned)k; VectorSize++){
-                new (Pointer + VectorSize) UsingType();
-            }
-        }
-        UsingType & Obj = Pointer[k];
-        return Obj;
-    }
-
-    void PushBack(UsingType elem)
-    {
-        VectorSize++;
-        if (VectorSize * sizeof(UsingType) > MemorySize)
-            if (MemoryGrow())
-                abort();
-        Pointer[VectorSize] = elem;
-    }
-
-    int GetSize()
-    {
-        return VectorSize;
-    }
+    explicit vector();
+    explicit vector(unsigned int); //creat a vector with specified initial MemorySize
+    explicit vector(my::vector<UsingType> const & ); // copy a vector
+    ~vector();
+    void operator=(my::vector<UsingType> const &);
+    UsingType const & operator[](unsigned int const) const;
+    UsingType & operator[](unsigned int const);
+    void PushBack(UsingType);
+    int GetSize();
 };
+
+#include "classVector.hpp"
 
 #endif // CLASSVECTOR_H
---
+++ classVector.hpp
@@ -0,0 +1,108 @@
+#ifndef CLASSVECTOR_HPP
+#define CLASSVECTOR_HPP
+#include "classVector.h"
+
+template <class UsingType>
+void my::vector<UsingType>::MemoryGrow()
+{
+    UsingType* BufPointer;
+    BufPointer = (UsingType*) new char[MemorySize * 2];
+    memcpy(BufPointer, Pointer, MemorySize);
+    MemorySize = MemorySize * 2;
+    Pointer = BufPointer;
+}
+
+template <class UsingType>
+my::vector<UsingType>::vector(): VectorSize(0),
+                                 MemorySize(MIN_MEMORY_SIZE * sizeof(UsingType))
+{
+    Pointer = (UsingType*) new char[MemorySize];
+}
+
+template <class UsingType>
+my::vector<UsingType>::vector(unsigned int _MemorySize): VectorSize(0),
+                                                         MemorySize(_MemorySize * sizeof(UsingType))
+{
+    Pointer = (UsingType*) new char[MemorySize];
+}
+
+template <class UsingType>
+my::vector<UsingType>::vector(my::vector<UsingType> const & _vector): VectorSize(_vector.VectorSize),
+                                                                      MemorySize(_vector.MemorySize)
+{
+    Pointer = (UsingType*) new char[MemorySize];
+    for (unsigned int i = 0; i < VectorSize; i++){
+        new (Pointer + i) UsingType();
+        Pointer[i] = _vector[i];
+    }
+}
+
+template <class UsingType>
+my::vector<UsingType>::~vector()
+{
+    delete[] Pointer;
+}
+
+template <class UsingType>
+void my::vector<UsingType>::operator=(my::vector<UsingType> const & _vector)
+{
+    UsingType* BufPointer;
+    BufPointer = (UsingType*) new char[_vector.MemorySize];
+    delete[] Pointer;
+    Pointer = BufPointer;
+    MemorySize = _vector.MemorySize;
+    VectorSize = _vector.VectorSize;
+    for (unsigned int i = 0; i < VectorSize; i++){
+        new (Pointer + i) UsingType();
+        Pointer[i] = _vector[i];
+    }
+}
+
+template <class UsingType>
+UsingType const & my::vector<UsingType>::operator[](unsigned int const k) const
+{
+    UsingType const & Obj = Pointer[k];
+    return Obj;
+}
+
+template <class UsingType>
+UsingType & my::vector<UsingType>::operator[](unsigned int const k)
+{
+    if (k >= VectorSize){
+        try{
+            while (k * sizeof(UsingType) >= MemorySize)
+                MemoryGrow();
+        }
+        catch(bad_alloc){
+            abort();
+        }
+        for (;VectorSize <= k; VectorSize++){
+            new (Pointer + VectorSize) UsingType();
+        }
+    }
+    UsingType & Obj = Pointer[k];
+    return Obj;
+}
+
+template <class UsingType>
+void my::vector<UsingType>::PushBack(UsingType elem)
+{
+    VectorSize++;
+    if (VectorSize * sizeof(UsingType) > MemorySize){
+        try{
+            MemoryGrow();
+        }
+        catch(bad_alloc){
+            abort();
+        }
+    }
+    Pointer[VectorSize] = elem;
+}
+
+template <class UsingType>
+int my::vector<UsingType>::GetSize()
+{
+    return VectorSize;
+}
+
+#endif //CLASSVECTOR_HPP
