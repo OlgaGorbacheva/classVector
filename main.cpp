69912c5225df74398daec970a4b4623afa5cd23c
Olga <olga.gorbacheva@phystech.edu>
2013-05-22 00:35:22 +0400

    bool

--- main.cpp
+++ main.cpp
@@ -3,15 +3,21 @@
 
 int main()
 {
-    my::vector<int> k;
-    my::vector<int> l(100);
-    my::vector<int> v(k);
-    my::vector<double> r;
-    l = v;
-    for (int i = 0; i < 10000; i++)
-        k[i] = i;
-    l = k;
-    for (int i = 0; i < 10; i++)
-        cout << l[i] << endl;
+    my::vector<bool> k;
+    k.PushBack(false);
+    k.PushBack(false);
+    k.PushBack(true);
+    k.PushBack(false);
+    k.PushBack(false);
+    k.PushBack(true);
+    k.PushBack(false);
+    k.PushBack(false);
+    k.PushBack(true);
+    k.PushBack(false);
+    k.PushBack(false);
+    k.PushBack(true);
+    cout << endl;
+    for (int i = 0; i < k.GetSize(); i++)
+        cout << k[i] << endl;
     return 0;
 }
